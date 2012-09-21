239dd75ae918bc5d0bbac26725b83ba681a05eea
Andrey Usov <ownport@gmail.com>
2012-09-21 22:56:14 +0300

    changed routing approach, more simplified form

--- manage.py
+++ manage.py
@@ -6,58 +6,29 @@
 
 import os
 import sys
-import logging
-
 import settings
+
 from packages import bottle
-from packages import pyservice
+from packages import pywebase
 
+# -----------------------------------------------
+#   Application functions
+# -----------------------------------------------
 
-# monkey patching for BaseHTTPRequestHandler.log_message
-def log_message(obj, format, *args):
-    logging.info("%s %s" % (obj.address_string(), format%args))
+bottle.route('/', 'GET', pywebase.handle_index)
+bottle.route('/favicon.ico', 'GET', pywebase.handle_favicon)
+bottle.route('/static/<filepath:path>', 'GET', pywebase.handle_static)
 
 # -----------------------------------------------
 #   Application
 # -----------------------------------------------
 
-# favicon.ico handling
-@bottle.get('/favicon.ico')
-def handle_favicon():
-    return bottle.static_file('images/favicon.ico', root=settings.STATIC_PATH)
-
-# static files handling
-@bottle.route('/static/<filepath:path>')
-def handle_static(filepath):
-    return bottle.static_file(filepath, root=settings.STATIC_PATH)
-    
-@bottle.get('/')
-@bottle.view('index')
-def handle_index():
-    return dict(name='pywebase')
-
-# -----------------------------------------------
-# Process to run
-class PywebaseProcess(pyservice.Process):
-
-    pidfile = settings.PYWEBASE_PIDFILE
-    logfile = settings.PYWEBASE_LOGFILE
-
-    def __init__(self):
-        super(PywebaseProcess, self).__init__()
-        
-        from BaseHTTPServer import BaseHTTPRequestHandler
-        BaseHTTPRequestHandler.log_message = log_message
-            
-    def run(self):
-        logging.info('pywebase {} server starting up'.format(bottle.__version__))
-        bottle.TEMPLATE_PATH.append(settings.TEMPLATE_PATH)
-        bottle.run(host='localhost', port=8080, debug=settings.DEBUG_MODE)
-
 if __name__ == '__main__':
+    
+    from packages import pyservice
 
     if len(sys.argv) == 2 and sys.argv[1] in 'start stop restart status'.split():
-        pyservice.service('manage.PywebaseProcess', sys.argv[1])
+        pyservice.service('packages.pywebase.PywebaseProcess', sys.argv[1])
     else:
         print 'usage: manager <start,stop,restart,status>'
 
---
+++ packages/pywebase.py
@@ -0,0 +1,49 @@
+#
+#   pywebase core
+#
+
+import logging
+import settings
+
+from packages import bottle
+from packages import pyservice
+
+# monkey patching for BaseHTTPRequestHandler.log_message
+def log_message(obj, format, *args):
+    logging.info("%s %s" % (obj.address_string(), format%args))
+
+# -----------------------------------------------
+# Process to run
+# -----------------------------------------------
+class PywebaseProcess(pyservice.Process):
+
+    pidfile = settings.PYWEBASE_PIDFILE
+    logfile = settings.PYWEBASE_LOGFILE
+
+    def __init__(self):
+        super(PywebaseProcess, self).__init__()
+        
+        from BaseHTTPServer import BaseHTTPRequestHandler
+        BaseHTTPRequestHandler.log_message = log_message
+            
+    def run(self):
+        logging.info('pywebase {} server starting up'.format(bottle.__version__))
+        bottle.TEMPLATE_PATH.append(settings.TEMPLATE_PATH)
+        bottle.run(host='localhost', port=8080, debug=settings.DEBUG_MODE)
+
+# -----------------------------------------------
+# Basic functions
+# -----------------------------------------------
+
+# favicon.ico handling
+def handle_favicon():
+    return bottle.static_file('images/favicon.ico', root=settings.STATIC_PATH)
+
+# static files handling
+def handle_static(filepath):
+    return bottle.static_file(filepath, root=settings.STATIC_PATH)
+    
+# main/index page
+def handle_index():
+    return bottle.template('index', dict(name='pywebase'))
+
